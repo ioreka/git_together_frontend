898c0adb541da76c1eee2864814753ce1b68fbdd
github username <github email address>
2018-08-17 06:54:37 +0100

    Add map

--- git-together/src/components/Map.js
+++ git-together/src/components/Map.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
 
 class Map extends React.Component {
+
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  }
+
   render() {
     return (
-      <div>MAP
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyAhlNg9SyzsjkZk-9rTBDC8BthNPMbH-uc"}}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+        </GoogleMapReact>
       </div>
     )
   }
